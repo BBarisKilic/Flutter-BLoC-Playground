b4b09f15666903bf5cdec73e3fda3015a1980758
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-06-23 23:25:46 +0300

    BLoCPattern completed.

---
+++ 01-BLoCPattern/lib/counter_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/counter_event.dart';
+
+class CounterBloc {
+  int _counter = 0;
+
+  final _counterStateController = StreamController<int>();
+  StreamSink<int> get _inCounter => _counterStateController.sink;
+
+  Stream<int> get counter => _counterStateController.stream;
+
+  final _counterEventController = StreamController<CounterEvent>();
+
+  Sink<CounterEvent> get counterEventSink => _counterEventController.sink;
+
+  CounterBloc() {
+    _counterEventController.stream.listen(_mapEventToState);
+  }
+
+  void _mapEventToState(CounterEvent event) {
+    if (event is IncrementEvent)
+      _counter++;
+    else
+      _counter--;
+
+    _inCounter.add(_counter);
+  }
+
+  void dispose() {
+    _counterStateController.close();
+    _counterEventController.close();
+  }
+}
